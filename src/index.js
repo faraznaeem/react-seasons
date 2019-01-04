8a71bb701b63bffaf50fd6982644fe568a76e3e5
faraznaeem <faraznaeem@gmail.com>
2019-01-04 08:17:06 +0100

    Update component did mount

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position), (err) => console.log(err)
-    );
+class App extends React.Component {
 
-    return (
-        <div>Hello </div>
+    // The component is initialized with the constructor function
 
-    )
-};
+    state = { lat: null, long: null }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude.toFixed(2),
+                                        long: position.coords.longitude.toFixed(2),
+                                        errorMessage: ''
+                                     }),
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('My component was updated');
+    }
+
+    // Always define the render function.
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return (
+                <div>
+                    <div>Latitude: {this.state.lat}</div>
+                    <div>Longitude: {this.state.long}</div>
+                </div>
+            );
+        }
+        return <div>Loading!</div>;
+    };
+}
 
 ReactDOM.render(<App/>, document.querySelector('#root'))
